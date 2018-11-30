cb9fcc8b9d760f73c9679398f0850d2717a80f81
Andrew <andrewjacob@utexas.edu>
2018-11-29 19:32:23 -0600

    copied functions over

---
+++ plagiarismCatcher.cpp
@@ -0,0 +1,40 @@
+//
+// Created by Andrew on 11/29/2018.
+//
+#include <sys/types.h>
+#include <dirent.h>
+#include <errno.h>
+#include <vector>
+#include <string>
+#include <iostream>
+
+using namespace std;
+int getdir(string dir, vector<string> &files);
+
+int main(int argc, char* argv[]){
+    string dir = string("sm_doc_set");
+    vector<string> files = vector<string>();
+
+    getdir(dir,files);
+
+    for (unsigned int i = 0;i < files.size();i++) {
+        cout << i << files[i] << endl;
+    }
+    return 0;
+}
+
+int getdir (string dir, vector<string> &files)
+{
+    DIR *dp;
+    struct dirent *dirp;
+    if((dp  = opendir(dir.c_str())) == NULL) {
+        cout << "Error(" << errno << ") opening " << dir << endl;
+        return errno;
+    }
+
+    while ((dirp = readdir(dp)) != NULL) {
+        files.push_back(string(dirp->d_name));
+    }
+    closedir(dp);
+    return 0;
+}
